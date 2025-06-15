b04048699738b08db3afab34ff70590cdc8cecd1
VIAAN96 <viaan1207@gmail.com>
2025-06-15 08:04:45 +0530

    AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,9 +1,7 @@
-#
-# Copyright (C) 2024 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_larry.mk
 
+COMMON_LUNCH_CHOICES := \
+    lineage_larry-user \
+    lineage_larry-userdebug \
+    lineage_larry-eng
